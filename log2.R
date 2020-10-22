3cf3cdfcc3724f784fb966d40cd5fce1da4b8755
Enrico R. Crema <enrico.crema@gmail.com>
2020-10-22 21:20:10 +0100

    Fixed missing bits on log file

--- log2.R
+++ log2.R
@@ -4,8 +4,8 @@ source(here('src','sampleCounts.R'))
 x = read.csv(here('oxcalresults','MCMC_uniform.csv'))
 x = x[,-c(1,ncol(x))] # eliminate pass number and last column
 cntMatrix = sampleCounts(x) #this will take a couple of minutes
-png(file='testPlot.png',width = 480,height=480)
-plot(cntMatrix)
+png(file=here('figures','testPlot.png'),width = 480,height=480)
+plotCntMat(cntMatrix)
 dev.off()
 
 
