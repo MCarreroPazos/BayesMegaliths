3c3e8946e34eb939bdbf9b2c0c44136829ced060
Enrico R. Crema <enrico.crema@gmail.com>
2020-10-22 21:18:41 +0100

    Sample workflow and plot

---
+++ log2.R
@@ -0,0 +1,12 @@
+library(rcarbon)
+library(here)
+source(here('src','sampleCounts.R'))
+x = read.csv(here('oxcalresults','MCMC_uniform.csv'))
+x = x[,-c(1,ncol(x))] # eliminate pass number and last column
+cntMatrix = sampleCounts(x) #this will take a couple of minutes
+png(file='testPlot.png',width = 480,height=480)
+plot(cntMatrix)
+dev.off()
+
+
+
